3b4e603e6a7a0e254b42a91d2a6b981fd553cd56
Mohammad Hojjat <hojjatjanjan@gmail.com>
2026-01-13 21:05:58 +0430

    initial the project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className={'flex-center h-screen'}>
+            <h1 className={'text-3xl text-indigo-300'}>Hello, GSAP</h1>
+        </div>
+    );
+}
